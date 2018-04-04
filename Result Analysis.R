ee7139cd83824f71c4cd36aeedc9f40b1fe37080
GlebFu <gleb.furman@gmail.com>
2018-04-03 19:56:40 -0500

    Results

--- Result Analysis.R
+++ Result Analysis.R
@@ -9,6 +9,62 @@ get_RPB <- function(X, T) {
   
 }
 
+get_RMSE <- function(X, T) {
+  sqrt(mean((X - T)^2))
+}
+
+pull_results <- function(cond, res.df) {
+  out.i <- list()
+  
+  for(i in 1:max(cond$CID)) {
+    # Read results
+    res.cond.i <- bind_rows(lapply(res.df$dirs[res.df$cond == i], read_csv))
+    
+    # Get Estimates
+    res.est.i <- res.cond.i %>%
+      filter(stat == "Estimate") %>%
+      select(X_PS.cor, M.m, u0.sd, Bs, ICC, isEqual:RE_ei) %>%
+      gather(key = Parameter, value = value, FE_Int:RE_ei)
+    
+    res.err.i <- res.cond.i %>%
+      filter(stat == "Error") %>%
+      select(X_PS.cor, M.m, u0.sd, Bs, ICC, isEqual:RE_ei) %>%
+      gather(key = Parameter, value = value, FE_Int:RE_ei)
+    
+    # Get true values
+    # Formula %>% cond[i,"gen_frm"]
+    true.est.i <- eval(parse(text = cond[i,"Bs"]))
+    true.est.i[6] <- mean(res.cond.i$u0.sd)^2
+    true.est.i <- tibble(stat = "Estimate", Parameter = res.cond.i %>% select(FE_Int:RE_ei) %>% names(), Truth = true.est.i)
+    
+    true.err.i <- res.est.i %>%
+      filter(stat == "Estimate") %>%
+      group_by(isRandom, isEqual, Gen, Parameter) %>%
+      summarise(Truth = sd(value),
+                stat = "Error")
+    
+    # Combine
+    res.i <- rbind(full_join(res.err.i, true.err.i), full_join(res.est.i, true.est.i))
+    
+    # Calculate outcomes
+    out.i[[i]] <- res.i %>%
+      group_by(X_PS.cor, M.m, u0.sd, Bs, ICC, isEqual, isRandom, Gen, stat, Parameter) %>%
+      summarise(RPB = get_RPB(value, Truth),
+                RMSE = get_RMSE(value, Truth))
+    
+    
+    # res.i %>%
+    #   group_by(X_PS.cor, M.m, u0.sd, Bs, ICC, isEqual, isRandom, Gen, stat, Parameter) %>% 
+    #   filter(stat == "Estimate",
+    #          Parameter == "RE_u0j") %>%
+    #   summarise(rpb = (mean(value) - mean(Truth))/mean(Truth),
+    #             rpb2 = get_RPB(value, Truth))
+    
+  }
+  
+  return(out.i)
+}
+
 cond <- read_csv("Results/2018-03-30/conditions.csv")[,-1]
 
 res.files <- list.files(path = "Results/2018-03-30")[str_detect(list.files(path = "Results/2018-03-30"), "Results")]
@@ -21,52 +77,12 @@ res.df <- tibble(files = res.files, dirs = res.dirs) %>%
 
 cond.manipulated <- names((cond %>% mutate_all(as.factor) %>% sapply(function(x) length(levels(x))))[(cond %>% mutate_all(as.factor) %>% sapply(function(x) length(levels(x)))) > 1])
 
-out.i <- list()
+# rm(list = "cond.manipulated", "res.dirs", "res.files")
 
-for(i in 1:max(cond$CID)) {
-  # Read results
-  res.cond.i <- bind_rows(lapply(res.df$dirs[res.df$cond == i], read_csv))
-  
-  # Get Estimates
-  res.est.i <- res.cond.i %>%
-    filter(stat == "Estimate") %>%
-    select(X_PS.cor, M.m, u0.sd, Bs, ICC, isEqual:RE_ei) %>%
-    gather(key = Parameter, value = value, FE_Int:RE_ei)
-  
-  res.err.i <- res.cond.i %>%
-    filter(stat == "Error") %>%
-    select(X_PS.cor, M.m, u0.sd, Bs, ICC, isEqual:RE_ei) %>%
-    gather(key = Parameter, value = value, FE_Int:RE_ei)
-  
-  # Get true values
-  # Formula %>% cond[i,"gen_frm"]
-  true.est.i <- eval(parse(text = cond[i,"Bs"]))
-  true.est.i[6] <- mean(res.cond.i$u0.sd)^2
-  true.est.i <- tibble(stat = "Estimate", Parameter = res.cond.i %>% select(FE_Int:RE_ei) %>% names(), Truth = true.est.i)
-  
-  true.err.i <- res.est.i %>%
-    filter(stat == "Estimate") %>%
-    group_by(isRandom, isEqual, Gen, Parameter) %>%
-    summarise(Truth = sd(value),
-              stat = "Error")
-  
-  # Combine
-  res.i <- rbind(full_join(res.err.i, true.err.i), full_join(res.est.i, true.est.i))
-  
-  # Calculate outcomes
-  out.i[[i]] <- res.i %>%
-    group_by(X_PS.cor, M.m, u0.sd, Bs, ICC, isEqual, isRandom, Gen, stat, Parameter) %>%
-    summarise(RPB = get_RPB(value, Truth))
-  
-  
-  # res.i %>%
-  #   group_by(X_PS.cor, M.m, u0.sd, Bs, ICC, isEqual, isRandom, Gen, stat, Parameter) %>% 
-  #   filter(stat == "Estimate",
-  #          Parameter == "RE_u0j") %>%
-  #   summarise(rpb = (mean(value) - mean(Truth))/mean(Truth),
-  #             rpb2 = get_RPB(value, Truth))
-    
-}
+# out.i <- pull_results(cond, res.df)
+# 
+# save(out.i, file = "Results/Brief/out.rdata")
+load("Results/Brief/out.rdata")
 
 results <- bind_rows(out.i) %>%
   ungroup() %>%
@@ -75,9 +91,11 @@ results <- bind_rows(out.i) %>%
          Gen = ifelse(Gen == "Y_EQ", "Equal", "Unequal"),
          Correct = (Est_Equal == Gen & Est_Random == "Random"),
          Correct = ifelse(Correct, T, NA),
-         Parameter = factor(Parameter))
+         Parameter = factor(Parameter),
+         Bs = factor(Bs))
 
 levels(results$Parameter) <- c("FE_Int", "FE_M", "FE_Mj", "FE_Xj", "FE_X", "RE_e", "RE_u")
+levels(results$Bs) <- c("Weak L2", "Strong L2")
 
 df_sum <- results %>% filter(stat == "Estimate")
 # df_sum <- results %>% filter(stat == "Error")
